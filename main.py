5f245ab67c6a4908ce587e255617a352ed7ea6a9
Joseph Lee <jslee5413@gmail.com>
2024-02-09 13:08:13 -0500

    initial

---
+++ main.py
@@ -0,0 +1,59 @@
+import pygame
+from sys import exit
+import math
+from settings import * 
+
+pygame.init()
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Escape from Klemensk")
+clock = pygame.time.Clock()
+
+background = pygame.transform.scale(pygame.image.load("background/background.png").convert(), (WIDTH, HEIGHT))
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.transform.rotozoom(pygame.image.load("player/player/0.png").convert_alpha(), 0, 0.35)
+        self.pos = pygame.math.Vector2(PLAYER_START_X, PLAYER_START_Y)
+
+    def user_input(self):
+        self.velocity_x = 0 
+        self.velocity_y = 0 
+     
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_w]:
+            self.velocity_y = -self.speed
+        if keys[pygame.K_s]:
+            self.velocity_y = self.speed
+        if keys[pygame.K_d]:
+            self.velocity_x = self.speed
+        if keys[pygame.K_a]:
+            self.velocity_x = -self.speed
+
+    def move(self):
+        self.pos.x += self.velocity_x 
+        self.pos.y += self.velocity_y
+
+    def update(self):
+        self.user_input()
+        self.move()
+
+
+player = Player()
+
+while True:
+    keys = pygame.key.get_pressed()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+
+    screen.blit(background, (0,0))
+    screen.blit(player.image, player.pos)
+    player.update()
+
+    pygame.display.update()
+    clock.tick(FPS)
