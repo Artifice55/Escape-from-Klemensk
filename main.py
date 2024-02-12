6e953cf881673ab5c23a03d2248e09b4a77d32e5
Anton Khassanov <marioanton9@gmail.com>
2024-02-12 14:48:05 -0500

    Finished Background and character movment

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Escape from Klemensk")
 clock = pygame.time.Clock()
 
-background = pygame.transform.scale(pygame.image.load("background/background.png").convert(), (WIDTH, HEIGHT))
+background = pygame.transform.scale(pygame.image.load("street_background.png").convert(), (WIDTH, HEIGHT))
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.transform.rotozoom(pygame.image.load("player/player/0.png").convert_alpha(), 0, 0.35)
+        self.image = pygame.transform.rotozoom(pygame.image.load("0.png").convert_alpha(), 0, 0.35)
         self.pos = pygame.math.Vector2(PLAYER_START_X, PLAYER_START_Y)
+        self.speed = PLAYER_SPEED 
 
     def user_input(self):
         self.velocity_x = 0 
@@ -33,8 +34,7 @@ class Player(pygame.sprite.Sprite):
             self.velocity_x = -self.speed
 
     def move(self):
-        self.pos.x += self.velocity_x 
-        self.pos.y += self.velocity_y
+        self.pos += pygame.math.Vector2(self.velocity_x, self.velocity_y)
 
     def update(self):
         self.user_input()
